afd70fe2a0b65c40643605101a18aea48f6687bc
solmii <rkdthfal4759@naver.com>
2020-09-02 17:07:50 +0900

    [Add] Android 에서 firebase 연동 및 console.firebase를 통해 push notification 테스트 완료

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
-import React, {useState} from 'react';
+import React, { useEffect } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
   ScrollView,
   StatusBar,
   TouchableOpacity,
-  View,
+  Alert,
   Text,
 } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      const status = remoteMessage.data.status;
+      const followerName = remoteMessage.data.followerName;
+      if (status === 'change') {
+        Alert.alert(
+          'Check it out right now!',
+          `The asset information of ${followerName} you are following has changed.`,
+        );
+      }
+      console.log(remoteMessage);
+    });
+    return unsubscribe;
+  }, []);
+
+  // useEffect(() => {
+  //   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+  //     Alert.alert('제발 되라 제발~~', JSON.stringify(remoteMessage));
+  //   });
+  //   return unsubscribe;
+  // }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
         <StatusBar translucent barStyle="dark-content" />
         <Text>Hi!!!</Text>
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.mainBtn}>
           <Text>Button1!!</Text>
         </TouchableOpacity>
       </SafeAreaView>
@@ -29,6 +52,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
+  },
+  mainBtn: {
+    padding: 14,
+    borderRadius: 10,
+    backgroundColor: 'skyblue',
   },
 });
 
--- index.js
+++ index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  const status = remoteMessage.data.status;
+  const followerName = remoteMessage.data.followerName;
+  if (status === 'change') {
+    Alert.alert(
+      'Check it out right now!',
+      `The asset information of ${followerName} you are following has changed.`,
+    );
+  }
+  console.log(remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
